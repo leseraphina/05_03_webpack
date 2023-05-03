de1ae3dcaa66289a23615cb556c1c7f089148479
leseraphina <leseraphina@gamil.com>
2023-05-03 18:00:20 +0900

    webpack start

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// build 정보
+// path 설정
+
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports={
+  entry:'./js/main.js',
+  output:{
+    // path:path.resolve(__dirname,'dist'),
+    // filename:'main.js',
+    clean:true
+  },
+  module:{
+    rules:[
+      {test:/\.s?css$/,
+      use:['style-loader','css-loader','sass-loader']}
+    ]
+  },
+  plugins:[
+    new HtmlPlugin({
+      template:'index.html'
+    }),
+    new CopyPlugin(
+      {patterns:[
+        {from:'static'}
+      ]}
+    )
+  ]
+}
